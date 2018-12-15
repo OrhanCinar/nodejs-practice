a4ac7f679e4a01718d09e6b4e5252dabffbf744f
Orhan CINAR <orhancinar35@gmail.com>
2018-12-15 02:28:06 +0100

    joi validation

--- joi_validation.js
+++ joi_validation.js
@@ -4,39 +4,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const Joi = require('joi');
 
-const arraystring = ['banana', 'cheese', 'something'];
-const arrayObjects = [{ example: 'example1' }, { example: 'example2' }];
-
-const userInput = {
-    personalInfo: {
-        streetAddress: '53456464',
-        city: 'NYC',
-        state: 'ntc'
-    },
-    preferences: arrayObjects
-};
-
-const personalInfoSchema = Joi.object().keys({
-    streetAddress: Joi.string().trim().required(),
-    city: Joi.string().trim().required(),
-    state: Joi.string().trim().min(2).required(),
-});
-
-const preferencesSchema = Joi.array().items(Joi.string());
-
-const schema = Joi.object().keys({
-    personalInfo: personalInfoSchema,
-    preferences: preferencesSchema
-})
-
-Joi.validate(userInput, schema, (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-});
-
 //dynamic binding
 app.use('/public', express.static(path.join(__dirname, 'static')));
 

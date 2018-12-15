69f02d1b96c4d22e18811717e2e10d2c1ee1d764
Orhan CINAR <orhancinar35@gmail.com>
2018-12-15 01:37:06 +0100

    add post form

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 //dynamic binding
 app.use('/public', express.static(path.join(__dirname, 'static')));
 
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    res.send('posted');
 });
 
 app.get('/example', (req, res) => {
